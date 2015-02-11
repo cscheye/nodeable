81532ff61c81f380a80d10f0d194615707955667
Cristi Scheye <cristi.scheye@gmail.com>
2015-02-10 19:29:12 -0800

    setting up server on port 3000

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var users = require('./routes/users');
 
 var app = express();
 
+var server = app.listen(3000, function(){
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Listening at http://%s:%s', host, port)
+})
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
